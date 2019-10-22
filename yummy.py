0bdcc1f8bbf40c0ace04bcf21a0d406697352840
Julian syn <jts319@ic.ac.uk>
2019-10-22 10:32:51 +0100

    testing

--- yummy.py
+++ yummy.py
@@ -1,3 +1,11 @@
 print("hello there!")
 
 print("I want to learn html and Java now.")
+
+words = ("bad", "badminton", "badass", "good", "happy", "joy")
+
+for item in words:
+  if "bad" in item:
+    continue
+  else:
+    print(item)
